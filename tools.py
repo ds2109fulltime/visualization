5dec74a0eff46d1fde1305bf6d468749d027e88f
diegoisla <diego.isla@outlook.com>
2021-12-14 00:09:52 +0100

    Add print_classif_metrics function

--- tools.py
+++ tools.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.metrics import roc_curve, r2_score, mean_absolute_error, mean_squared_error
+from sklearn.metrics import roc_curve, r2_score, mean_absolute_error, mean_squared_error, accuracy_score, f1_score, precision_score, recall_score,\
+                            roc_auc_score, roc_curve
 
 def get_roc_curve(y, y_pred, ax=None, **plt_kwargs):
 # def get_roc_curve(y, y_pred, ax=None):
@@ -27,3 +28,11 @@ def print_regress_metrics(y, y_pred):
     y_array, y_pred_array = np.array(y), np.array(y_pred)
     mape = np.mean(np.abs((y_array - y_pred_array) / y_array)) * 100
     print(f'MAPE score: {round(mape, 4)} %')
+
+
+def print_classif_metrics(y, y_pred):
+    print(f'Accuracy score: {round(accuracy_score(y_pred, y), 3)} %')
+    print(f'Recall score: {round(recall_score(y_pred, y), 3)} %')
+    print(f'Precision score: {round(precision_score(y_pred, y), 3)} %')
+    print(f'F1 score: {round(f1_score(y_pred, y), 3)} %')
+    print(f'AUC: {round(roc_auc_score(y_pred, y), 3)} %')
