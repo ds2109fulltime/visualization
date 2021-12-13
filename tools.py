f1b9e7832a0fe7416575f55173d07b53a31553af
diegoisla <diego.isla@outlook.com>
2021-12-13 23:57:10 +0100

    Add print regression metrics function

--- tools.py
+++ tools.py
@@ -1,5 +1,6 @@
+import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_curve, r2_score, mean_absolute_error, mean_squared_error
 
 def get_roc_curve(y, y_pred, ax=None, **plt_kwargs):
 # def get_roc_curve(y, y_pred, ax=None):
@@ -16,3 +17,13 @@ def get_roc_curve(y, y_pred, ax=None, **plt_kwargs):
         #    grid = True
     )
     return(ax)
+
+
+def print_regress_metrics(y, y_pred):
+    print("R^2 score:", round(r2_score(y_pred, y), 4))
+    print("MAE score:", round(mean_absolute_error(y_pred, y), 4))
+    print("MSE score:", round(mean_squared_error(y_pred, y), 4))
+    print("RMSE score:", round(np.sqrt(mean_squared_error(y_pred, y)), 4))
+    y_array, y_pred_array = np.array(y), np.array(y_pred)
+    mape = np.mean(np.abs((y_array - y_pred_array) / y_array)) * 100
+    print(f'MAPE score: {round(mape, 4)} %')
