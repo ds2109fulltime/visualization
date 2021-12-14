2908f4690159d181564f8314c11a98c5bc227cb9
diegoisla <diego.isla@outlook.com>
2021-12-14 17:57:52 +0100

    Edit roc curve function

--- tools.py
+++ tools.py
@@ -1,23 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.metrics import roc_curve, r2_score, mean_absolute_error, mean_squared_error, accuracy_score, f1_score, precision_score, recall_score,\
                             roc_auc_score, roc_curve
 
-def get_roc_curve(y, y_pred, ax=None, **plt_kwargs):
-# def get_roc_curve(y, y_pred, ax=None):
-    if ax is None:
-        ax = plt.gca()
-    fpr, tpr, thresholds = roc_curve(y, y_pred)
-    ax.plot(fpr, tpr, **plt_kwargs)
-    # ax.plot(fpr, tpr)
+def show_roc_curve(y, y_pred, style = 'seaborn', figsize=(10,5), extra_title = ''):
+    ''' 
+    This function plot the ROC curve for a classification model predicts
+        
+    Args:
+        y (pandas.Series): The real target values.
+        y_pred (pandas.Series): The target values predicted by the model.
+        style (str): Here we define the graph style we want to use. 
+            To see all the available styles, please type this: print(plt.style.available)
+        figsize (tuple): Here we define the graph size, it has to be a tuple with 2 values
+        extra_title (str): An extra text added to the title
+    
+    Returns:
+        None
+
+    '''
+
+    fpr, tpr, thresholds = roc_curve(y, y_pred, )
+
+    plt.style.use(style)
+    plt.figure(figsize=figsize)
+
+    ax = sns.lineplot(fpr, tpr)
     ax.set(xlim = [0.0, 1.0],
            ylim = [0.0, 1.0],
-           title = 'ROC curve for titanic classifier',
+           title = 'ROC curve ' + extra_title,
            xlabel = 'False Positive Rate (1 - Specificity)',
            ylabel = 'True Positive Rate (Sensitivity)',
-        #    grid = True
     )
-    return(ax)
+    plt.show()
 
 
 def print_regress_metrics(y, y_pred):
